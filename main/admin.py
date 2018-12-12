d0cb79713f3592abde70f7918a76c2e47a43317a
RDavis2005 <RDavis2005@msn.com>
2018-12-12 07:09:16 -0500

    Add Post and Comments to Admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from main.models import Post, Comment, User
 
 # Register your models here.
+
+class CommentInline(admin.StackedInline):
+    model = Comment
+    fields = ("text",)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("text",)
+    inlines = [
+        CommentInline,
+    ]
+
+admin.site.register(User)
+admin.site.register(Post, PostAdmin)
--- main/models.py
+++ main/models.py
@@ -11,6 +11,9 @@ class Post(models.Model):
     date_posted = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.text
+
 class Comment(models.Model):
     post = models.ForeignKey(to=Post, on_delete=models.CASCADE, related_name="comments")
     text = models.CharField(max_length=200)
