75e326401c2afa4c4ac16d363a177a1a40a2a42b
RDavis2005 <RDavis2005@msn.com>
2018-12-12 06:41:51 -0500

    Create User and Comment models

--- main/models.py
+++ main/models.py
@@ -4,3 +4,15 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="posts")
+    text = models.CharField(max_length=200)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(to=Post, on_delete=models.CASCADE, related_name="comments")
+    text = models.CharField(max_length=200)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
