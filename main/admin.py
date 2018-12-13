2a5d1fcdb4c178621184264f7e29ca43670884cd
RDavis2005 <RDavis2005@msn.com>
2018-12-12 23:03:38 -0500

    Add Following model to Admin

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from main.models import Post, Comment, User
+from main.models import Post, Comment, User, Following
 
 # Register your models here.
 
@@ -7,11 +7,20 @@ class CommentInline(admin.StackedInline):
     model = Comment
     fields = ("text",)
 
+class FollowersInline(admin.StackedInline):
+    model = Following
+    fk_name = "followed_user"
+    fields = ("following_user",)
+
+class UserAdmin(admin.ModelAdmin):
+    fields = ("username", "email", "is_superuser", "is_staff", "is_active",)
+    inlines = [FollowersInline]
+
 class PostAdmin(admin.ModelAdmin):
     list_display = ("text",)
     inlines = [
         CommentInline,
     ]
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Post, PostAdmin)
