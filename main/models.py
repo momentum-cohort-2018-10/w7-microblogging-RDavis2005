0350dddbe14d79eb9e6317ce28bd3cd2def74e40
RDavis2005 <RDavis2005@msn.com>
2018-12-12 22:00:18 -0500

    Create following class, update user class and run migration

--- main/models.py
+++ main/models.py
@@ -3,10 +3,15 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    pass
+    users_followed = models.ManyToManyField(
+        to="User", through="Following", through_fields=("following_user", "followed_user"), related_name="following") #Who the user is following
+
+class Following(models.Model):
+    following_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following_from")   #Who is doing the following
+    followed_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following_to")      #Who is being followed
 
 class Post(models.Model):
-    owner = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="posts")
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name="posts") #A post can only have one owner
     text = models.CharField(max_length=200)
     date_posted = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
@@ -15,7 +20,7 @@ class Post(models.Model):
         return self.text
 
 class Comment(models.Model):
-    post = models.ForeignKey(to=Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(to=Post, on_delete=models.CASCADE, related_name="comments") #A post can have many comments
     text = models.CharField(max_length=200)
     date_posted = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
